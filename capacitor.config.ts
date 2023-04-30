488ca11b75069434542d5feba4ca43748cd71759
ShingoTanikawa <shingo.tanikawa@o-n.co.jp>
2023-04-30 11:01:50 +0900

    2nd

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.proub.cap4',
+  appName: 'preact-starter',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
